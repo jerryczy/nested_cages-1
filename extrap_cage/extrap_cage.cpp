8d9687ddfd18041c95812ece1554cea3abf55773
Simeon Krastnikov <simeon.krastnikov@mail.utoronto.ca>
2018-12-06 12:54:54 -0500

    Account for changes in normals

--- extrap_cage/extrap_cage.cpp
+++ extrap_cage/extrap_cage.cpp
@@ -15,14 +15,15 @@ void extrap_cage(
   V2_C = V1_C.replicate(1, 1);
   Eigen::MatrixXd V2_C_inc = Eigen::MatrixXd::Zero(V2_C.rows(), 3);
   
-  Eigen::MatrixXd N;
-  igl::per_vertex_normals(V1, F, N);
+  Eigen::MatrixXd N1, N2;
+  igl::per_vertex_normals(V1, F, N1);
+  igl::per_vertex_normals(V2, F, N2);
   
   Eigen::VectorXd total_vertex_weights = Eigen::VectorXd::Zero(V2_C.rows());
   
   for (int vi = 0; vi < V1.rows(); vi++) {
     igl::Hit hit;
-    igl::ray_mesh_intersect(V1.row(vi), N.row(vi), V1_C, F_C, hit);
+    igl::ray_mesh_intersect(V1.row(vi), N1.row(vi), V1_C, F_C, hit);
     Eigen::MatrixXd bc(1, 3);
     bc << hit.id, hit.u, hit.v;
     Eigen::RowVector3i coarse_face = F_C.row(hit.id);
@@ -34,9 +35,10 @@ void extrap_cage(
     Eigen::RowVector3d l;
     igl::barycentric_coordinates(coarse_point, a, b, c, l);
     
-    V2_C_inc.row(coarse_face[0]) += l[0] * (V2.row(vi) - V1.row(vi));
-    V2_C_inc.row(coarse_face[1]) += l[1] * (V2.row(vi) - V1.row(vi));
-    V2_C_inc.row(coarse_face[2]) += l[2] * (V2.row(vi) - V1.row(vi));
+    Eigen::RowVector3d t = V2.row(vi) - V1.row(vi) + N2.row(vi) * (coarse_point - V1.row(vi)).norm();
+    V2_C_inc.row(coarse_face[0]) += l[0] * t;
+    V2_C_inc.row(coarse_face[1]) += l[1] * t;
+    V2_C_inc.row(coarse_face[2]) += l[2] * t;
     
     total_vertex_weights[coarse_face[0]] += l[0];
     total_vertex_weights[coarse_face[1]] += l[1];
